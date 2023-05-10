c9d5515dec0cadd931d5a29b44eb6cc6d19e59dc
topurayhan007 <topurayhan007@gmail.com>
2023-05-10 13:45:11 +0600

    api create, middleware

---
+++ __steps__.js
@@ -0,0 +1,9 @@
+/**
+ * 1. create a post api on the server side
+ * 2. send data from client side using post
+ * 3. set fetch method inside the fetch options (second parameter)
+ * 4. options will have three things: method
+ * 5. options will have headers: "content-type": "application/json"
+ * 6. don't forget to send data by JSON.Stringify in the body
+ * 7. On Server Side: don't forget to use express.json() middleware
+ */
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
+app.use(express.json());
 
 const users = [
   { id: 1, name: "Sabana", email: "sabana@gmail.com" },
@@ -18,6 +19,15 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
   res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  console.log("post api hitting");
+  console.log(req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
 });
 
 app.listen(port, () => {
